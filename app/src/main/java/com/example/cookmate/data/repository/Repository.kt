5b246a46f87602fac996d19004ced0aac4eef05d
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-17 17:53:57 +0300

    implement RemoteDataSource with repository

--- app/src/main/java/com/example/cookmate/data/repository/Repository.kt
+++ app/src/main/java/com/example/cookmate/data/repository/Repository.kt
@@ -1,11 +1,18 @@
 package com.example.cookmate.data.repository
 
 import com.example.cookmate.data.local.entity.FavouriteRecipeEntity
+import com.example.cookmate.data.model.CategoryDto
+import com.example.cookmate.data.model.MealDto
 
 interface Repository {
 
     suspend fun addFavouriteRecipe(recipe: FavouriteRecipeEntity)
     suspend fun getAllFavouriteRecipes(): List<FavouriteRecipeEntity>
     suspend fun removeFavouriteRecipe(recipeId: Int)
+
+    suspend fun recipeSearch(name: String): List<MealDto.Recipe>
+    suspend fun getMealById(id: Int): List<MealDto.Recipe>
+    suspend fun getAllCategories(): List<CategoryDto.CategoriesItem>
+    suspend fun getRandomMeal(): List<MealDto.Recipe>
 
 }
