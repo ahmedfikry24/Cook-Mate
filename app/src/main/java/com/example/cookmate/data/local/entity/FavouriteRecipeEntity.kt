9c7aef2f65eee0821f2e01822d0f4b20fce4f5a0
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-17 17:49:52 +0300

    implement retrofitService functions && add models

--- app/src/main/java/com/example/cookmate/data/local/entity/FavouriteRecipeEntity.kt
+++ app/src/main/java/com/example/cookmate/data/local/entity/FavouriteRecipeEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "favouriteRecipeEntity")
 data class FavouriteRecipeEntity(
     @PrimaryKey
-    val id: Int,
+    val id: String,
     val name: String,
     val type: String,
     val area: String,
--- app/src/main/java/com/example/cookmate/data/remote/RetrofitServices.kt
+++ app/src/main/java/com/example/cookmate/data/remote/RetrofitServices.kt
@@ -1,8 +1,21 @@
 package com.example.cookmate.data.remote
 
-import retrofit2.Response
+import com.example.cookmate.data.model.CategoryDto
+import com.example.cookmate.data.model.MealDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitServices {
 
+    @GET("search.php")
+    suspend fun recipeSearch(@Query("s") name: String): List<MealDto.Recipe>
+
+    @GET("lookup.php")
+    suspend fun getMealById(@Query("i") id: Int): List<MealDto.Recipe>
+
+    @GET("categories.php")
+    suspend fun getAllCategories(): List<CategoryDto.CategoriesItem>
+
+    @GET("random.php")
+    suspend fun getRandomMeal(): List<MealDto.Recipe>
 }
