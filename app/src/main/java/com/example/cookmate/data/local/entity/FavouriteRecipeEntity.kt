123f4a9f253e11c6eff3d22215dfe724325ce2b4
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-17 16:19:51 +0300

    prepare room && implement Dao

---
+++ app/src/main/java/com/example/cookmate/data/local/entity/FavouriteRecipeEntity.kt
@@ -0,0 +1,14 @@
+package com.example.cookmate.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favouriteRecipeEntity")
+data class FavouriteRecipeEntity(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val type: String,
+    val area: String,
+    val imageUrl: String,
+)
