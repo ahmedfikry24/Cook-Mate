824d42008c5696ea7cf72b395e63b44d7b15300d
Mostafa-Mohamed4932 <m.mohammed4932@gmail.com>
2024-08-18 20:57:00 +0300

    Imported stuff for LoginFragment file

--- app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
@@ -1,11 +1,21 @@
 package com.example.cookmate.ui.login
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cookmate.R
+import com.example.cookmate.data.local.RoomManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
