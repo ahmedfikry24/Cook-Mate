da65a95773a2a3b52f1a09e12e55c7102d33375d
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-18 21:58:44 +0300

    setup bottomNav with navGraph

--- app/src/main/java/com/example/cookmate/RecipeActivity.kt
+++ app/src/main/java/com/example/cookmate/RecipeActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class RecipeActivity : AppCompatActivity() {
+    private lateinit var navHost: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,12 @@ class RecipeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        navHost = supportFragmentManager.findFragmentById(R.id.user_main_container) as NavHostFragment
+        navController = navHost.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
