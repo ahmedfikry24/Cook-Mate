b90b1375fed295a9479b032506a2a9527d6b0160
Mostafa-Mohamed4932 <m.mohammed4932@gmail.com>
2024-08-19 11:34:58 +0300

    Fixed an unused import directive

--- app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
@@ -1,6 +1,6 @@
 package com.example.cookmate.ui.login
 
-import android.content.Context
+//import android.content.Context
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
