04216346e3decc9bdb73d116dd268805747df154
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-20 20:52:10 +0300

    init observers && views

--- app/src/main/java/com/example/cookmate/ui/favourite/FavouriteFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/favourite/FavouriteFragment.kt
@@ -6,25 +6,62 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cookmate.R
 import com.example.cookmate.data.local.RoomManager
 import com.example.cookmate.data.remote.RetrofitManager
 import com.example.cookmate.data.repository.RepositoryImpl
 import com.example.cookmate.data.source.LocalDataSourceImpl
 import com.example.cookmate.data.source.RemoteDataSourceImpl
+import com.example.cookmate.ui.favourite.adapter.FavoriteAdapter
+import com.example.cookmate.ui.favourite.view_model.FavoriteEvents
 import com.example.cookmate.ui.favourite.view_model.FavoriteViewModel
 import com.example.cookmate.ui.favourite.view_model.FavoriteViewModelFactory
 
 class FavouriteFragment : Fragment() {
+    private lateinit var recycler: RecyclerView
+    private lateinit var favoriteAdapter: FavoriteAdapter
+
     private val remoteDataSource by lazy { RemoteDataSourceImpl(RetrofitManager.service) }
     private val localDataSource by lazy { LocalDataSourceImpl(RoomManager.getInit(requireContext())) }
     private val repository by lazy { RepositoryImpl(remoteDataSource, localDataSource) }
     private val viewModel by viewModels<FavoriteViewModel> { FavoriteViewModelFactory(repository) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getFavoriteRecipes()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_favourite, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews(view)
+        viewModelObserver()
+    }
+
+    private fun initViews(view: View) {
+        recycler = view.findViewById(R.id.favorite_recycler)
+        favoriteAdapter = FavoriteAdapter(listOf())
+        recycler.adapter = favoriteAdapter
+    }
+
+    private fun viewModelObserver() {
+        viewModel.favoriteRecipes.observe(viewLifecycleOwner) { recipes ->
+            favoriteAdapter.updateRecipes(recipes)
+        }
+        viewModel.events.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                FavoriteEvents.Idle -> Unit
+                is FavoriteEvents.OnClickFavorite -> viewModel.removeFavoriteRecipe(event.id)
+                is FavoriteEvents.OnClickItem -> {}
+            }
+            viewModel.events.postValue(FavoriteEvents.Idle)
+        }
     }
 }
