39957ac5c8e7ea7be3d820e965d959a6906d1e79
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-21 15:01:16 +0300

    add favorite adapter

---
+++ app/src/main/java/com/example/cookmate/ui/home/adapters/HomeFavoriteAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.cookmate.ui.home.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.cookmate.R
+import com.example.cookmate.ui.home.view_model.RecipeInfo
+import com.example.cookmate.ui.utils.loadImageUrl
+
+class HomeFavoriteAdapter(
+    var recipes: List<RecipeInfo>,
+) : RecyclerView.Adapter<HomeFavoriteAdapter.HomeFavoriteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeFavoriteViewHolder {
+        return HomeFavoriteViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.home_favorite_item, parent, false)
+        )
+    }
+
+    override fun getItemCount() = recipes.size
+
+    override fun onBindViewHolder(holder: HomeFavoriteViewHolder, position: Int) {
+        val item = recipes[position]
+        holder.apply {
+            image.loadImageUrl(item.url)
+            text.text = item.name
+            itemView.setOnClickListener { item.onClick(item.id) }
+        }
+    }
+
+    fun updateRecipes(newItems: List<RecipeInfo>) {
+        val diffUtil = DiffUtil.calculateDiff(
+            MainAdapterDiffUtil(
+                recipes,
+                newItems,
+                areItemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+                areContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+            ))
+        recipes = newItems.take(5)
+        diffUtil.dispatchUpdatesTo(this)
+    }
+
+    class HomeFavoriteViewHolder(view: View) : ViewHolder(view) {
+        val image: AppCompatImageView = view.findViewById(R.id.image_recipe)
+        val text: TextView = view.findViewById(R.id.title_recipe)
+    }
+
+}
