71e30091f345562598ba15efd68a62dddf37923b
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-23 14:50:27 +0300

    refactor home with bases

--- app/src/main/java/com/example/cookmate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/home/HomeFragment.kt
@@ -1,66 +1,37 @@
 package com.example.cookmate.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookmate.R
-import com.example.cookmate.data.local.RoomManager
-import com.example.cookmate.data.remote.RetrofitManager
-import com.example.cookmate.data.repository.RepositoryImpl
-import com.example.cookmate.data.source.LocalDataSourceImpl
-import com.example.cookmate.data.source.RemoteDataSourceImpl
+import com.example.cookmate.ui.base.BaseFragment
 import com.example.cookmate.ui.home.adapters.MainAdapter
 import com.example.cookmate.ui.home.view_model.HomeEvents
 import com.example.cookmate.ui.home.view_model.HomeViewModel
-import com.example.cookmate.ui.home.view_model.HomeViewModelFactory
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel>() {
+
     private lateinit var mainRecycler: RecyclerView
     private lateinit var mainAdapter: MainAdapter
     private lateinit var progressBar: ProgressBar
 
-    private val remoteDataSource by lazy { RemoteDataSourceImpl(RetrofitManager.service) }
-    private val localDataSource by lazy { LocalDataSourceImpl(RoomManager.getInit(requireContext())) }
-    private val repository by lazy { RepositoryImpl(remoteDataSource, localDataSource) }
-    private val viewModel by viewModels<HomeViewModel> { HomeViewModelFactory(repository) }
-
-    private lateinit var navController: NavController
+    override val fragmentId = R.layout.fragment_home
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.getDate()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initViews(view)
-        viewModelObservers()
-    }
-
-    private fun initViews(view: View) {
+    override fun initViews(view: View) {
         mainRecycler = view.findViewById(R.id.main_recycler)
         mainAdapter = MainAdapter(listOf(), listOf(), viewModel)
         mainRecycler.adapter = mainAdapter
         progressBar = view.findViewById(R.id.home_progress_bar)
-        navController = findNavController()
     }
 
-    private fun viewModelObservers() {
+    override fun viewModelObservers() {
         viewModel.categories.observe(viewLifecycleOwner) { categories ->
             progressBar.isVisible = categories.isEmpty()
             mainRecycler.isVisible = categories.isNotEmpty()
--- app/src/main/java/com/example/cookmate/ui/home/view_model/HomeViewModel.kt
+++ app/src/main/java/com/example/cookmate/ui/home/view_model/HomeViewModel.kt
@@ -2,17 +2,10 @@ package com.example.cookmate.ui.home.view_model
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.cookmate.data.repository.Repository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
-class HomeViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return modelClass.getConstructor(Repository::class.java).newInstance(repository)
-    }
-}
 
 class HomeViewModel(
     private val repository: Repository,
