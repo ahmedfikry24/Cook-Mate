c38b859b4c994f0168a1f290fb44667f73b43e64
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-24 00:35:07 +0300

    remove Login boilerplate code

--- app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/login/LoginFragment.kt
@@ -1,85 +1,42 @@
 package com.example.cookmate.ui.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cookmate.R
-import com.example.cookmate.data.local.RoomManager
 import com.example.cookmate.data.local.shared_pref.SharedPrefManager
-import com.example.cookmate.data.remote.RetrofitManager
-import com.example.cookmate.data.repository.RepositoryImpl
-import com.example.cookmate.data.source.LocalDataSourceImpl
-import com.example.cookmate.data.source.RemoteDataSourceImpl
+import com.example.cookmate.ui.base.BaseFragment
+import com.example.cookmate.ui.login.view_model.LoginViewModel
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment<LoginViewModel>() {
 
     private lateinit var nameInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var signInButton: Button
     private lateinit var signUpTextView: TextView
 
-    private val localDataSource by lazy { LocalDataSourceImpl(RoomManager.getInit(requireContext())) }
-    private val remoteDataSource by lazy { RemoteDataSourceImpl(RetrofitManager.service) }
-    private val repository by lazy { RepositoryImpl(remoteDataSource, localDataSource) }
-    private val viewModel by viewModels<LoginViewModel> { LoginViewModelFactory(repository) }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Initialize Shared Preferences if not already done
-        SharedPrefManager.init(requireContext())
-
-        // Check if the user is already logged in
-        if (SharedPrefManager.isLogin) {
-            navigateToHome()
-            return
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
+    override val fragmentId = R.layout.fragment_login
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initViews(view)
-        observeViewModel()
-
-        signInButton.setOnClickListener {
-            viewModel.onLoginClicked(
-                nameInput.text.toString(),
-                passwordInput.text.toString()
-            )
-        }
-
-        signUpTextView.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
+        initListeners()
     }
 
-    private fun initViews(rootView: View) {
-        nameInput = rootView.findViewById(R.id.nameInput)
-        passwordInput = rootView.findViewById(R.id.passwordInput)
-        signInButton = rootView.findViewById(R.id.signInButton)
-        signUpTextView = rootView.findViewById(R.id.signUp)
+    override fun initViews(view: View) {
+        nameInput = view.findViewById(R.id.nameInput)
+        passwordInput = view.findViewById(R.id.passwordInput)
+        signInButton = view.findViewById(R.id.signInButton)
+        signUpTextView = view.findViewById(R.id.signUp)
     }
 
-    private fun observeViewModel() {
+    override fun viewModelObservers() {
         viewModel.inputError.observe(viewLifecycleOwner) { errorMessage ->
             errorMessage?.let {
-                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                showToast(it)
                 viewModel.clearInputError()
             }
         }
@@ -89,14 +46,26 @@ class LoginFragment : Fragment() {
                 SharedPrefManager.isLogin = true
                 navigateToHome()
             } else {
-                Toast.makeText(requireContext(), "Wrong name or password, please try again.", Toast.LENGTH_SHORT).show()
+                showToast("Wrong name or password, please try again.")
             }
         }
+    }
+
+    private fun initListeners() {
+        signInButton.setOnClickListener {
+            viewModel.onLoginClicked(nameInput.text.toString(), passwordInput.text.toString())
+        }
+
+        signUpTextView.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_registerFragment) }
     }
 
     private fun navigateToHome() {
         val navController = findNavController()
         navController.setGraph(R.navigation.user_nav_graph)
         navController.navigate(R.id.homeFragment)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
