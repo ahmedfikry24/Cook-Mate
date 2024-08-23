82aafd70b95456ca97286bd8c34f2c648af29ad7
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-24 00:47:21 +0300

    refactor SearchFragment with bases

--- app/src/main/java/com/example/cookmate/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/cookmate/ui/search/SearchFragment.kt
@@ -1,93 +1,75 @@
 package com.example.cookmate.ui.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookmate.R
-import com.example.cookmate.data.local.RoomManager
-import com.example.cookmate.data.remote.RetrofitManager
-import com.example.cookmate.data.repository.RepositoryImpl
-import com.example.cookmate.data.source.LocalDataSourceImpl
-import com.example.cookmate.data.source.RemoteDataSourceImpl
+import com.example.cookmate.ui.base.BaseFragment
 
-class SearchFragment : Fragment() {
+class SearchFragment : BaseFragment<SearchViewModel>() {
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var searchView: androidx.appcompat.widget.SearchView
+    private lateinit var searchView: SearchView
 
-    private val remoteDataSource by lazy { RemoteDataSourceImpl(RetrofitManager.service) }
-    private val localDataSource by lazy { LocalDataSourceImpl(RoomManager.getInit(requireContext())) }
-    private val repository by lazy { RepositoryImpl(remoteDataSource, localDataSource) }
-    private val viewModel by viewModels<SearchViewModel> { SearchViewModelFactory(repository) }
+    override val fragmentId = R.layout.fragment_search
 
-    private val recipeAdapter by lazy { RecipeAdapter { recipeId ->
-        val action = SearchFragmentDirections.actionSearchFragmentToRecipeDetailsFragment(recipeId)
-        findNavController().navigate(action)
-    } }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    private val recipeAdapter by lazy {
+        RecipeAdapter { recipeId ->
+            val action =
+                SearchFragmentDirections.actionSearchFragmentToRecipeDetailsFragment(recipeId)
+            findNavController().navigate(action)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews(view)
-        observeViewModel()
+        initListeners()
     }
 
-    private fun initViews(view: View) {
+    override fun initViews(view: View) {
         recyclerView = view.findViewById(R.id.recipesRecyclerView)
         searchView = view.findViewById(R.id.searchView)
-
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         recyclerView.adapter = recipeAdapter
-
-        // Ensure the whole search bar is clickable
-        searchView.setOnClickListener {
-            searchView.isIconified = false // Open up the search bar when clicked
-            viewModel.onSearchViewClicked(searchView.query.toString()) // Forward click handling to ViewModel
-        }
-
-        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                query?.let { viewModel.onSearchViewClicked(it) } // Forward to ViewModel
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean = false
-        })
     }
 
-    private fun observeViewModel() {
+    override fun viewModelObservers() {
         viewModel.recipes.observe(viewLifecycleOwner) { recipes ->
             recipeAdapter.updateData(recipes)
             if (recipes.isEmpty()) {
-                viewModel.notifyUser("No recipes found") // Notify using ViewModel
+                viewModel.notifyUser("No recipes found")
             }
         }
-
         viewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
             errorMessage?.let {
                 viewModel.notifyUser(it)
             }
         }
-
-        // Observe UI events like showing a Toast
         viewModel.uiEvent.observe(viewLifecycleOwner) { event ->
             event?.let {
                 Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
-                viewModel.clearUiEvent() // Clear event after showing it
+                viewModel.clearUiEvent()
             }
         }
+    }
+
+    private fun initListeners() {
+        searchView.setOnClickListener {
+            searchView.isIconified = false
+            viewModel.onSearchViewClicked(searchView.query.toString())
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { viewModel.onSearchViewClicked(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean = false
+        })
     }
 }
