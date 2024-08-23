fc6fe48cd4fe80e8bc0193bff7e4960c4265cc0e
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-23 16:12:37 +0300

    achieve encapsulation principle in DetailsViewModel

--- app/src/main/java/com/example/cookmate/ui/recipe_details/view_model/RecipeDetailsViewModel.kt
+++ app/src/main/java/com/example/cookmate/ui/recipe_details/view_model/RecipeDetailsViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.cookmate.ui.recipe_details.view_model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cookmate.data.local.entity.FavouriteRecipeEntity
 import com.example.cookmate.data.repository.Repository
+import com.example.cookmate.ui.shared_ui_state.RecipeUiState
+import com.example.cookmate.ui.shared_ui_state.toEntity
+import com.example.cookmate.ui.shared_ui_state.toUiState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -12,35 +15,29 @@ class RecipeDetailsViewModel(
     private val repository: Repository,
 ) : ViewModel() {
 
-    val recipe = MutableLiveData<RecipeDetails>()
-    var isFavorite: Boolean = false
+    private val _recipe = MutableLiveData<RecipeUiState>()
+    val recipe: LiveData<RecipeUiState> = _recipe
+
+    private var _isFavorite: Boolean = false
+    val isFavorite = _isFavorite
 
     fun getRecipeInfo(id: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val result = repository.getMealById(id)
-            isFavorite = repository.getAllFavouriteRecipes().any { it.id == id }
+            _isFavorite = repository.getAllFavouriteRecipes().any { it.id == id }
             if (result.isNotEmpty())
-                recipe.postValue(result.map { it.toUiState(isFavorite) }.first())
+                _recipe.postValue(result.map { it.toUiState(isFavorite) }.first())
         }
     }
 
     fun onClickFavorite() {
         viewModelScope.launch {
-            val value = recipe.value
             if (isFavorite) {
                 repository.removeFavouriteRecipe(recipe.value?.id ?: "")
             } else {
-                repository.addFavouriteRecipe(
-                    FavouriteRecipeEntity(
-                        id = value?.id ?: "",
-                        name = value?.name ?: "",
-                        type = value?.category ?: "",
-                        area = value?.area ?: "",
-                        imageUrl = value?.imageUrl ?: ""
-                    )
-                )
+                recipe.value?.toEntity()?.let { repository.addFavouriteRecipe(it) }
             }
-            isFavorite = !isFavorite
+            _isFavorite = !isFavorite
         }
     }
 }
