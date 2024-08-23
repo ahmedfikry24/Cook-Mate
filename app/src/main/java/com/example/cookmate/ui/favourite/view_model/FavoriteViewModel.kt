5c772719693f1f85f71496226c3783a24b21f2cc
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-23 15:43:21 +0300

    achieve encapsulation principle in FavoriteViewModel

--- app/src/main/java/com/example/cookmate/ui/favourite/view_model/FavoriteViewModel.kt
+++ app/src/main/java/com/example/cookmate/ui/favourite/view_model/FavoriteViewModel.kt
@@ -1,45 +1,30 @@
 package com.example.cookmate.ui.favourite.view_model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.cookmate.data.repository.Repository
+import com.example.cookmate.ui.shared_ui_state.RecipeUiState
+import com.example.cookmate.ui.shared_ui_state.toUiState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
-class FavoriteViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return modelClass.getConstructor(Repository::class.java).newInstance(repository)
-    }
-}
 
 class FavoriteViewModel(
     private val repository: Repository,
 ) : ViewModel() {
 
-    val favoriteRecipes = MutableLiveData<List<FavoriteRecipeInfo>>(listOf())
-    val events = MutableLiveData<FavoriteEvents>(FavoriteEvents.Idle)
+    private val _favoriteRecipes = MutableLiveData<List<RecipeUiState>>(listOf())
+    val favoriteRecipes: LiveData<List<RecipeUiState>> = _favoriteRecipes
+
+    private val _events = MutableLiveData<FavoriteEvents>(FavoriteEvents.Idle)
+    val events: LiveData<FavoriteEvents> = _events
 
     fun getFavoriteRecipes() {
         viewModelScope.launch(Dispatchers.Default) {
             val result = repository.getAllFavouriteRecipes()
-            favoriteRecipes.postValue(
-                result.map {
-                    it.toUiState(
-                        onClickItem = {
-                            viewModelScope.launch {
-                                events.postValue(FavoriteEvents.OnClickItem(it))
-                            }
-                        },
-                        onClickFavorite = {
-                            viewModelScope.launch {
-                                events.postValue(FavoriteEvents.OnClickFavorite(it))
-                            }
-                        }
-                    )
-                }
-            )
+            _favoriteRecipes.postValue(result.map { it.toUiState() })
         }
     }
 
@@ -47,7 +32,7 @@ class FavoriteViewModel(
     fun removeFavoriteRecipe(id: String) {
         viewModelScope.launch(Dispatchers.Default) {
             repository.removeFavouriteRecipe(id)
-            favoriteRecipes.postValue(
+            _favoriteRecipes.postValue(
                 favoriteRecipes.value?.toMutableList()?.filterNot { it.id == id }
             )
         }
